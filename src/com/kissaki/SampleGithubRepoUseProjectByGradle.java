917b71350c724c3ba931ff259e93c43e210d238f
sassembla <sassembla@hotmail.com>
2012-07-27 00:32:49 +0900

    add resources

---
+++ src/com/kissaki/SampleGithubRepoUseProjectByGradle.java
@@ -0,0 +1,15 @@
+package com.kissaki;
+
+import com.kissaki.sample.*;
+
+public class SampleGithubRepoUseProjectByGradle {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SampleMavenRepoByGradle m = new SampleMavenRepoByGradle();
+		m.something();
+	}
+
+}
